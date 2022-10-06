b848983864da21e84503050a2c82338402b5e5e5
Asti <astistateofmind@gmail.com>
2022-10-06 10:17:49 -0700

    add data.js and seed.js files

---
+++ seed.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+require('./config/database');
+
+// const Asana = require('./models/asana')
+
+// const data = require('./data')
+
+// const p1 = Asana.deleteMany({})
+// Promise.all([p1])
+// .then(function(result) {
+//     console.log(result)
+//     return Promise.all([
+//         Asana.create(data.asanas)
+//     ])
+// })
+// .then(process.exit)
