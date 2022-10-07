997426228c53491e28717dd63dcebe03f58a9fd3
Asti <astistateofmind@gmail.com>
2022-10-07 10:41:53 -0700

    create models for soup, grandma, veggie

---
+++ models/veggie.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const veggiesSchema = new Schema({
+    photo: [
+        {
+            type: String,
+            validate: {
+                validator: function (value) {
+                    const urlPattern = /(http|https):\/\/(\w+:{0,1}\w*#)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%#!\-/]))?/;
+                    const urlRegExp = new RegExp(urlPattern);
+                    return value.match(urlRegExp);
+            },
+                message: props => `${props.value} is not a valid URL`
+            }
+        }
+    ],
+    label: {
+        type: String,
+        required: true
+    },
+    ingredients: [{
+        type: String,
+        required: true
+    }],
+    stepOne: {
+        type: String,
+        required: true
+    },
+    stepTwo: {
+        type: String,
+        required: true
+    },
+    stepThree: {
+        type: String,
+        required: true
+    },
+    stepFour: {
+        type: String,
+        required: true
+    },
+    stepFive: {
+        type: String,
+        required: true
+    },
+    fullRecipe: {
+        type: String,
+    }
+
+})
+
+module.exports = mongoose.model('Veggie', veggiesSchema)
